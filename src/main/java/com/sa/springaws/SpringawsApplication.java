44b4f026557cdcbd1455342d3c0418b5f7a1512e
unknown <hwy16016@gmail.com>
2022-09-21 01:20:05 +0900

    Upload app completed by chapter 2

---
+++ src/main/java/com/sa/springaws/SpringawsApplication.java
@@ -0,0 +1,13 @@
+package com.sa.springaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringawsApplication.class, args);
+	}
+
+}
