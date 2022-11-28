b53595a43dcdd9b9e1367b71c24c258d821eb19c
siriyaoff <hwy16016@gmail.com>
2022-11-29 01:03:08 +0900

    fix(scripts/start.sh): fix typo

--- scripts/start.sh
+++ scripts/start.sh
@@ -25,7 +25,7 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
-echo "> $JAR_NAME 을 profile=$IDLE_PROFILE 로 실행합니다."
+echo "> $JAR_NAME 을 profile-$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=$IDLE_PROFILE \
